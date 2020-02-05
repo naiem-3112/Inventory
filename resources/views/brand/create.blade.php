d1e0eb70ce842777c71cb047341971c650e2595b
Shahadat Hossain <naiem.3112@gmail.com>
2020-02-05 12:41:52 +0600

    brand migration created

--- resources/views/brand/create.blade.php
+++ resources/views/brand/create.blade.php
@@ -4,7 +4,7 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-sm-2">
-                    <h3>Add Product</h3>
+                    <h3>Add Brand</h3>
                 </div>
             </div>
         </div>
@@ -19,35 +19,10 @@
                     <div class="box box-success col-lg-12">
                         <form>
                             <div class="form-group">
-                                <label>Product Name</label>
-                                <input type="text" name="productName" class="form-control" placeholder="Enter product name">
+                                <label>Brand Name</label>
+                                <input type="text" name="brandName" class="form-control" placeholder="Enter brand name">
                             </div>
-                            <div class="form-group">
-                                <label>Brand</label>
-                                <select name="brand_id" class="form-control">
-                                    <option selected disabled>Select Brand</option>
-                                    <option value="">brand 1</option>
-                                    <option value="">brand 2</option>
-                                </select>
-                            </div>
-                            <div class="form-group">
-                                <label>Category</label>
-                                <select name="category_id" class="form-control">
-                                    <option selected disabled>Select Category</option>
-                                    <option value="">category 1</option>
-                                    <option value="">category 2</option>
-                                </select>
-                            </div>
-
-                            <div class="form-group">
-                                <label>Buying Price</label>
-                                <input type="number" name="buyingPrice" class="form-control" placeholder="Enter product buying price">
-                            </div>
-                            <div class="form-group">
-                                <label>Selling Price</label>
-                                <input type="number" name="sellingPrice" class="form-control" placeholder="Enter product selling price">
-                            </div>
-                            <button type="submit" class="btn btn-primary" style="margin-bottom: 5px">Submit</button>
+                            <button type="submit" class="btn btn-primary" style="margin-bottom: 5px">Add Brand</button>
                         </form>
                     </div>
                 </div>
