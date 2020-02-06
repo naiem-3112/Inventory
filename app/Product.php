b72eebce738535b510c5ca93bf54de0bdd99353a
Shahadat Hossain <naiem.3112@gmail.com>
2020-02-06 16:08:50 +0600

    product relation with brand and category

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    public function brand(){
+        return $this->belongsTo(Brand::class);
+    }
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
 }
