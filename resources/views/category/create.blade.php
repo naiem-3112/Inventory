92c47abe529c83b3875de5513a3a343f94a63456
Shahadat Hossain <naiem.3112@gmail.com>
2020-02-06 14:26:02 +0600

    category creation done

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -1,25 +1,34 @@
 @extends('layouts.backend.master')
 @section('master.content')
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-sm-2">
-                    <h3>Add Category</h3>
-                </div>
-            </div>
-        </div>
-    </section>
-
     <section class="content">
         <div class="container-fluid">
             <div class="row">
                 <!--left-column-->
                 <div class="col-md-6 offset-sm-2">
                     <div class="box box-success col-lg-12">
-                        <form>
+                        <div class="box box-header"  style="font-size: 20px">
+                            <p class="pull-left">Add Category</p>
+                            <button class="pull-right btn btn-sm btn-success"><a style="color: #ffffff" href="{{ route('category.list') }}">View List</a></button>
+                        </div>
+                        <form action="{{ route('category.store') }}" method="post">
+                            @csrf
                             <div class="form-group">
                                 <label>Category Name</label>
                                 <input type="text" name="categoryName" class="form-control" placeholder="Enter category name">
+                                @if($errors->has('categoryName'))
+                                    <span class="error" style="color: red">{{ $errors->first('categoryName') }}</span>
+                                    @endif
+                            </div>
+                            <div class="form-group">
+                                <label>Status</label>
+                                <select class="form-control" name="status">
+                                    <option selected disabled>Select</option>
+                                    <option value="1">Active</option>
+                                    <option value="0">Inactive</option>
+                                </select>
+                                @if($errors->has('status'))
+                                    <span class="error" style="color: red">{{ $errors->first('status') }}</span>
+                                @endif
                             </div>
 
                             <button type="submit" class="btn btn-primary" style="margin-bottom: 5px">Add Category</button>
